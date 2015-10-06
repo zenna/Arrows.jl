604f09e7c769f2ca7e391839c59bc5247a41fd48
Zenna Tavares <zennatavares@gmail.com>
2015-10-06 16:46:40 -0400

    added theano target init

---
+++ src/compilation_targets/theano.jl
@@ -0,0 +1,22 @@
+## Compilation to Theano
+## =====================
+
+using PyCall
+@pyimport theano.compile.function as f
+@pyimport theano.tensor as T
+
+thfunction = f.zfunction
+
+x = pycall(T.dscalar, PyAny, "x")
+y = pycall(T.dscalar, PyAny, "y")
+z = x[:__add__](y)
+
+op = thfunction([x,y], z)
+
+"A Theano Function.  Represents a compiled arrow"
+immutable TheanoFunc
+  func::PyObject
+end
+
+function convert(TheanoFunc, funcdef::FuncDef)
+  
