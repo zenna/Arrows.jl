059a49a7217bf42c381c444e62a50f67a0d1b201
Javier Burroni <javier.burroni@gmail.com>
2017-09-15 19:18:39 -0400

    avoid syntax errors in `tests/propagate.jl`

--- test/propagate.jl
+++ test/propagate.jl
@@ -1,9 +1,9 @@
 using Arrows
 using NamedTuples
 
-function test_propagate
+function test_propagate()
   carr = TestArrows.xy_plus_x_arr()
-  x, y z = sub_prts(carr)
+  x, y, z = sub_prts(carr)
   # Suppose we know the output has shae (1,2,3)
   sprtvals = Dict(z => @NT(:shape = Shape((1,2,3))))
   propagate!(carr, shape)
