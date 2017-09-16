d96dc0e289c1c122a776611a6f3b4091f929273c
Javier Burroni <javier.burroni@gmail.com>
2017-09-16 15:16:38 -0400

    add required elements to `test/propagate.jl`

--- test/propagate.jl
+++ test/propagate.jl
@@ -1,12 +1,15 @@
 using Arrows
 using NamedTuples
+using Arrows.TestArrows
+using Base.Test
+
 
 function test_propagate()
   carr = TestArrows.xy_plus_x_arr()
-  x, y, z = sub_prts(carr)
-  # Suppose we know the output has shae (1,2,3)
+  x, y, z = sub_ports(carr)
+  # Suppose we know the output has shape (1,2,3)
   sprtvals = Dict(z => @NT(:shape = Shape((1,2,3))))
-  propagate!(carr, shape)
+  propagate!(carr, sprtvals)
   @test sprtvals[x].shape == Shape((1, 2, 3))
   @test sprtvals[y].shape == Shape((1, 2, 3))
 end
