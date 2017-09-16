40e828b54478e53aaa44f89efad52fffded0a406
Zenna Tavares <zennatavares@gmail.com>
2017-09-16 12:18:53 -0400

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,8 @@ using Documenter, Arrows
 makedocs()
 
 deploydocs(
-    repo = "github.com/zenna/Arrows.jl.git"
+    repo = "github.com/zenna/Arrows.jl.git",
+    julia="0.6",
+    deps=nothing,
+    make=nothing,
 )
