a258a01177ac9ce233aed7d4dc7abdf392964694
Javier Burroni <javier.burroni@gmail.com>
2017-09-16 16:21:55 -0400

    remove code duplication in `propagate!`

--- src/propagate/propagate.jl
+++ src/propagate/propagate.jl
@@ -46,6 +46,25 @@ function unpropagated_ports{T}(sarr::SubArrow, sprtvals::Dict{SubPort, T})
     filter(port -> !haskey(sprtvals, port), sub_ports(sarr))
 end
 
+"helper funtion to add values to the propagation"
+function add_value!{T}(prop::Propagation, port::SubPort, value::T)
+    prop.sprtvals[port] = value
+    push!(prop.pending, port)
+end
+
+"helper funtion to add values to the propagation while including `sub_arrow`"
+function add_value_arrow!{T}(prop::Propagation, port::SubPort, value::T)
+    add_value!(prop, port, value)
+    push!(prop.touched_arrows, sub_arrow(port))
+end
+
+"helper function that checks conflict during the propagation"
+function check_conflict{T}(prop::Propagation, port::SubPort, value::T)
+    if prop.sprtvals[port] != value
+        throw(DomainError(msg))
+    end
+end
+
 function propagate!{T}(carr:: CompArrow,
                        sprtvals::Dict{SubPort, T})::Dict{SubPort, T}
   !is_recursive(carr) || throw(DomainError())
@@ -57,30 +76,21 @@ function propagate!{T}(carr:: CompArrow,
       value = propagation.sprtvals[port]
       for ne in neighbors(port)
           if haskey(propagation.sprtvals, ne)
-              if propagation.sprtvals[ne] != value
-                  throw(DomainError())
-              end
+              check_conflict(propagation, ne, value)
           else
-              propagation.sprtvals[ne] = value
-              push!(propagation.pending, ne)
-              push!(propagation.touched_arrows, sub_arrow(ne))
+              add_value_arrow!(propagation, ne, value)
           end
       end
     end
     for touched in propagation.touched_arrows
-        selected_port = propagated_ports(touched, propagation.sprtvals)[1]
+        selected_port = first(propagated_ports(touched, propagation.sprtvals))
         value = propagation.sprtvals[selected_port]
         unpropagated = unpropagated_ports(touched, propagation.sprtvals)
-        if !isempty(unpropagated)
-            for port in unpropagated
-                push!(propagation.pending, port)
-                sprtvals[port] = value
-            end
+        for port in unpropagated
+            add_value!(propagation, port, value)
         end
         for port in sub_ports(touched)
-            if propagation.sprtvals[port] != value
-                throw(DomainError(msg))
-            end
+            check_conflict(propagation, port, value)
         end
     end
   end
