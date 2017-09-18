543317d8ef0e66cbf50171f85737eb4a4a81483d
autodocs <autodocs>
2017-09-18 07:11:11 +0000

    build based on 1d81dae

---
+++ latest/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Arrows.jl",
+    "title": "Arrows.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Arrows.jl-1",
+    "page": "Arrows.jl",
+    "title": "Arrows.jl",
+    "category": "section",
+    "text": "Arrows.jl is a differentiable programming environment implemented in Julia.  The goal is to combine the benefits of deep neural networks - namely, that they are differentiable, -  with the benefits of modern programming languages - recursion, modularity, higher-orderness, types. To do this, we build upon the formalism of Arrows."
+},
+
+{
+    "location": "index.html#Arrows.CompArrow",
+    "page": "Arrows.jl",
+    "title": "Arrows.CompArrow",
+    "category": "Type",
+    "text": "A Composite Arrow: An Arrow composed of multiple Arrows\n\n\n\n"
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Arrows.jl",
+    "title": "Installation",
+    "category": "section",
+    "text": "Arrows is built in Julia but not yet in the official Julia Package repository.  You can still easily install it from a Julia repl with:Pkg.clone(\"https://github.com/zenna/Arrows.jl.git\")CompArrow"
+},
+
+]}
