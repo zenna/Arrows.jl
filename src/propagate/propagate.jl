6a69b1485d06245fb0af4aa0a282f284100fa1e0
Javier Burroni <javier.burroni@gmail.com>
2017-09-20 22:11:00 -0400

    first version of `propagate!` that uses `SrcValue`

--- src/propagate/propagate.jl
+++ src/propagate/propagate.jl
@@ -1,13 +1,16 @@
 "Object to keep the state of the propagation process"
 mutable struct Propagation{T}
-  pending::Vector{SubPort}
+  pending::Set{SrcValue}
+  value_content::Dict{SrcValue, T}
   touched_arrows::Set
   sprtvals::Dict{SubPort, T}
   function Propagation{T}(seed::Dict{SubPort, T})
     p = new{T}()
     p.sprtvals = seed
-    p.pending = collect(keys(seed))
+    p.pending = Set(SrcValue(sport) for sport in keys(seed))
     p.touched_arrows = Set()
+    p.value_content = Dict(SrcValue(sport) => content
+                          for (sport, content) in seed)
     p
   end
 end
@@ -36,25 +39,36 @@ function propagate!{T}(carr:: CompArrow,
   #implements a difussion process over the network created by ports.
 end
 
-"helper function to filter already processed ports"
-function propagated_sports{T}(sarr::SubArrow, sprtvals::Dict{SubPort, T})
-  filter(sport -> haskey(sprtvals, sport), sub_ports(sarr))
+"helper function to filter already processed sports"
+function propagated_sports{T}(sarr::SubArrow, prop::Propagation{T})
+  filter(sport -> haskey(prop.sprtvals, sport), sub_ports(sarr))
 end
 
-"helper function to filter not yet processed ports"
-function unpropagated_sports{T}(sarr::SubArrow, sprtvals::Dict{SubPort, T})
-  filter(sport -> !haskey(sprtvals, sport), sub_ports(sarr))
+"helper function to filter already processed values"
+function propagated_values{T}(sarr::SubArrow, prop::Propagation{T})
+  sports = propagated_sports(sarr, prop)
+  Set(map(SrcValue, sports))
+end
+
+"helper function to filter not yet processed sports"
+function unpropagated_sports{T}(sarr::SubArrow, prop::Propagation{T})
+  filter(sport -> !haskey(prop.sprtvals, sport), sub_ports(sarr))
+end
+
+"helper function to filter not yet processed values"
+function unpropagated_values{T}(sarr::SubArrow, prop::Propagation{T})
+  sports = unpropagated_sports(sarr, prop)
+  Set(map(SrcValue, sports))
 end
 
 "helper funtion to add values to the propagation"
-function add_value!{T}(prop::Propagation, sport::SubPort, value::T)
-  prop.sprtvals[sport] = value
-  push!(prop.pending, sport)
+function add_content!{T}(prop::Propagation{T}, sport::SubPort, content::T)
+  prop.sprtvals[sport] = content
 end
 
 "helper funtion to add values to the propagation while including `sub_arrow`"
-function add_value_arrow!{T}(prop::Propagation, sport::SubPort, value::T)
-  add_value!(prop, sport, value)
+function add_content_arrow!{T}(prop::Propagation{T}, sport::SubPort, content::T)
+  add_content!(prop, sport, content)
   push!(prop.touched_arrows, sub_arrow(sport))
 end
 
@@ -63,24 +77,34 @@ end
   arrows/values. For instance, if we are propagating a shape, the behavior of a
   `MatrixMultArrow` is very different than `AddArrow` """
 function propagate_through!(prop::Propagation)
-  for touched in prop.touched_arrows
-    selected_sport = first(propagated_sports(touched, prop.sprtvals))
-    value = prop.sprtvals[selected_sport]
-    unpropagated = unpropagated_sports(touched, prop.sprtvals)
-    for sport in unpropagated
-      add_value!(prop, sport, value)
-    end
-    for sport in sub_ports(touched)
-      check_conflict(prop, sport, value)
+  for sarrow in prop.touched_arrows
+    selected_value = first(propagated_values(sarrow, prop))
+    content = prop.value_content[selected_value]
+    unpropagated = unpropagated_values(sarrow, prop)
+    for value in unpropagated
+      prop.value_content[value] = content
+      push!(prop.pending, value)
     end
   end
   prop.touched_arrows = Set()
 end
 
 "helper function that checks conflict during the propagation"
-function check_conflict{T}(prop::Propagation, sport::SubPort, value::T)
-  if prop.sprtvals[sport] != value
+function check_conflict{T}(prop::Propagation{T}, sport::SubPort, content::T)
+  if prop.sprtvals[sport] != content
     throw(DomainError(msg))
+  end
+end
+
+"helper function to propagate the content of a value to all its subports"
+function propagate!{T}(value::SrcValue, prop::Propagation{T})
+  content = prop.value_content[value]
+  for sport in sub_ports(value)
+    if haskey(prop.sprtvals, sport)
+      check_conflict(prop, sport, content)
+    else
+      add_content_arrow!(prop, sport, content)
+    end
   end
 end
 
@@ -90,15 +114,8 @@ function propagate!{T}(carr:: CompArrow,
   propagation = Propagation{T}(sprtvals)
   while !isempty(propagation.pending)
     while !isempty(propagation.pending)
-      sport = pop!(propagation.pending)
-      value = propagation.sprtvals[sport]
-      for ne in neighbors(sport)
-        if haskey(propagation.sprtvals, ne)
-          check_conflict(propagation, ne, value)
-        else
-          add_value_arrow!(propagation, ne, value)
-        end
-      end
+      value = pop!(propagation.pending)
+      propagate!(value, propagation)
     end
     propagate_through!(propagation)
   end
