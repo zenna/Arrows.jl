51803d74b10703d3a2e729218517e813abe41b7a
minasyan <minasyan@mit.edu>
2017-09-22 19:01:28 -0400

    pgf working, interactive test inside the file

--- src/transform/pgf.jl
+++ src/transform/pgf.jl
@@ -19,26 +19,29 @@ function pgf(arr::AddArrow)
 end
 
 pgf_rename!(carr::CompArrow) = (rename!(carr, Symbol(:pgf_, carr.name)); carr)
+
 pgf_in(sarr::SubArrow) = pgf(deref(sarr))
 pgf_out = pgf_rename! ∘ (carr -> link_to_parent!(carr, loose ∧ should_src))
-function pgflala!(carr::CompArrow)
+
+function pgf_change!(carr::CompArrow)
   for sarr in sub_arrows(carr)
     replarr, port_map = pgf_in(sarr), id_portid_map(deref(sarr))
     replace_sub_arr!(sarr, replarr, port_map)
   end
   pgf_out(carr)
 end
-pgf(carr::CompArrow) = pgflala!(deepcopy(carr))
 
-# carr = CompArrow(:test, [:x, :y], [:z])
-# x, y, z = sub_ports(carr)
-# (x * y + x) ⥅ z
-# carr
-#
-# inv_carr = invert(carr)
-# carr(1, 2)
-#
-# pgf_carr = pgf(carr)
-# pgf_carr(1, 2)
-#
-# inv_carr(3, 2, 1)
+pgf(carr::CompArrow) = pgf_change!(deepcopy(carr))
+
+carr = CompArrow(:test, [:x, :y], [:z])
+x, y, z = sub_ports(carr)
+(x * y + x) ⥅ z
+carr
+
+inv_carr = invert(carr)
+carr(1, 2)
+
+pgf_carr = pgf(carr)
+pgf_carr(1, 2)
+
+inv_carr(3, 2, 1)
