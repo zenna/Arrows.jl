72b5cc793c21eb225a1d69c6dd69e5e502700429
Javier Burroni <javier.burroni@gmail.com>
2017-11-02 17:13:53 -0400

    begin rewriting symbol execution

--- src/sym/sym.jl
+++ src/sym/sym.jl
@@ -1,93 +1,57 @@
-import SymPy: Sym, symbols, Eq, Ne, SympyTRUE
+SymUnion = Union{Expr, Array, Number, Symbol}
+
 
 "Refined Symbol {x | pred}"
 struct RefnSym
-  var::Sym
-  preds::Set{Sym} # Conjunction of predicates
+  var::SymUnion
+  preds::Set{} # Conjunction of predicates
 end
 
 "Unconstrained Symbol"
-RefnSym(sym::Sym) = RefnSym(sym, Set{Sym}())
-
-prim_sym_interpret{N}(::InvDuplArrow{N}, xs::Vararg{Sym, N})::Vector{Sym} =
-  [first(xs)]
-
-function domainpreds{N}(::InvDuplArrow{N}, xs::Vararg{Sym, N})
-  preds = Set{Sym}()
-  x1 = first(xs)
-  for (i, x) in enumerate(xs)
-    if i > 1
-      push!(preds, Eq(x, x1))
-    end
-  end
-  preds
-end
-
-function sym_interpret(x::SourceArrow, args)::Vector{RefnSym}
-  @show args
-  @show typeof(args)
-  @show typeof(x.value)
-  [RefnSym(Sym(x.value))]
-end
-
-prim_sym_interpret(::SubtractArrow, x, y)::Vector{Sym} = [x .- y,]
-prim_sym_interpret(::MulArrow, x, y)::Vector{Sym} = [x .* y,]
-prim_sym_interpret(::AddArrow, x, y)::Vector{Sym} = [x .+ y,]
-function prim_sym_interpret(::DivArrow, x, y)::Vector{Sym}
-  @show x
-  @show y
-  [x ./ y,]
-end
-prim_sym_interpret(::LogArrow, x)::Vector{Sym} = [log.(x),]
-# SymPy doesnt support it
-# domainpreds(::DivArrow, x, y) = Set{Sym}([Ne(y, 0)])
-domainpreds(::Arrow, args...) = Set{Sym}()
-
-function sym_interpret(parr::PrimArrow, args::Vector{RefnSym})::Vector{RefnSym}
-  vars = Sym[arg.var for arg in args]
-  preds = Set{Sym}[arg.preds for arg in args]
-  outputs = prim_sym_interpret(parr, vars...)
-  dompreds = domainpreds(parr, vars...)
-  allpreds = union(dompreds, preds...)
-  map(var -> RefnSym(var, allpreds), outputs)
-end
-
-sym_interpret(sarr::SubArrow, args) = sym_interpret(deref(sarr), args)
+RefnSym(sym::SymUnion) = RefnSym(sym, Set{SymUnion}())
 
 function Sym(prps::Props)
   # TODO: Add Type assumption
   ustring = string(name(prps))
-  symbols(filter(isascii, ustring))
+  Symbol(ustring)
 end
-
 Sym(prt::Port) = Sym(props(prt))
 RefnSym(prt::Port) = RefnSym(Sym(prt))
 
-# Recurse
+
+domainpreds(::Arrow, args...) = Set{SymUnion}()
+function domainpreds{N}(::InvDuplArrow{N}, xs::Vararg{SymUnion, N})
+  x1 = first(xs)
+  f = x-> :($(x) == $(x1))
+  Set{SymUnion}(map(f, xs[2:end]))
+end
+
++(x::SymUnion, y::SymUnion) = :($(x) + $(y))
+-(x::SymUnion, y::SymUnion) = :($(x) - $(y))
+/(x::SymUnion, y::SymUnion) = :($(x) / $(y))
+*(x::SymUnion, y::SymUnion) = :($(x) * $(y))
+
+prim_sym_interpret(::SubtractArrow, x, y) = [x .- y,]
+prim_sym_interpret(::MulArrow, x, y) = [x .* y,]
+prim_sym_interpret(::AddArrow, x, y) = [x .+ y,]
+prim_sym_interpret(::DivArrow, x, y) = [x ./ y,]
+prim_sym_interpret(::LogArrow, x) = [log.(x),]
+function prim_sym_interpret{N}(::InvDuplArrow{N},
+                                xs::Vararg{SymUnion, N})::Vector{SymUnion}
+  [first(xs)]
+end
+
+
+function sym_interpret(parr::PrimArrow, args::Vector{RefnSym})::Vector
+  vars = [arg.var for arg in args]
+  preds = Set[arg.preds for arg in args]
+  @show outputs = prim_sym_interpret(parr, vars...)
+  @show dompreds = domainpreds(parr, vars...)
+  @show allpreds = union(dompreds, preds...)
+  map(var -> RefnSym(var, allpreds), outputs)
+end
+
+
+sym_interpret(sarr::SubArrow, args) = sym_interpret(deref(sarr), args)
 sym_interpret(carr::CompArrow, args) =
   interpret(sym_interpret, carr, args)
-
-"Constraints on inputs to `carr`"
-function constraints(carr::CompArrow, remove=[SympyTRUE])
-  inp = map(RefnSym, ▸(carr))
-  outs = interpret(sym_interpret, carr, inp)
-  allpreds = Set{Sym}()
-  foreach(out -> union!(allpreds, out.preds), outs)
-  filter(pred -> pred ∉ remove, allpreds)
-end
-
-function solve!(class::Vector{Port})
-  carr = CompArrow(:test)
-
-  # If only one in class do nothing
-  if length(class) > 1
-    sprts = sub_port.(class)
-  end
-
-end
-
-function solve!(carr::CompArrow)
-  preds = constraints(invarr)
-  # solve equality constraints
-  θnms = Sym.(▸(invarr, is(θp)))
-end
