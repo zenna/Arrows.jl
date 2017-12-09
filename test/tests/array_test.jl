a14a296c14af7c9a1532e1be87c48ac5fd6e11f5
Javier Burroni <javier.burroni@gmail.com>
2017-12-08 22:47:09 -0500

    fix #99 create a function that computes the `mean` as we need it. Call that function when creating the julia expression

--- test/tests/array_test.jl
+++ test/tests/array_test.jl
@@ -31,7 +31,15 @@ function test_inv_dupl_arr()
   @test c(1:2, 1:2) == collect(1:2)
 end
 
-
+function test_mean_array()
+  c = CompArrow(:c, [:x, :y], [:z])
+  s = add_sub_arr!(c, MeanArrow{2}())
+  (c, 1) ⥅ (s, 1)
+  (c, 2) ⥅ (s, 2)
+  (s, 1) ⥅ (c, 1)
+  @test c([1, 2], [4,5]) == [2.5, 3.5]
+end
 
 test_gather_nd()
 test_inv_dupl_arr()
+test_mean_array()
