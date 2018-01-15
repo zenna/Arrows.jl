6bb418138cdf9f17bbfca73d0eb3c75b0cbbead3
Javier Burroni <javier.burroni@gmail.com>
2018-01-15 16:58:30 -0500

    simplify `inv_xor` implementation

--- src/library/inv_boolean.jl
+++ src/library/inv_boolean.jl
@@ -1,13 +1,10 @@
 "Inverse Xor"
 function inv_xor()
   c = CompArrow(:inv_xor, [:z, :θxor], [:x, :y])
-  z, θ = ▸(c)
-  x, y = ◂(c)
+  z, θ = ▹(c)
+  x, y = ◃(c)
   addprop!(θp, θ)
-  xor = add_sub_arr!(c, XorArrow())
-  z ⥅ (xor, 1)
-  θ ⥅ (xor, 2)
-  (xor, 1) ⥅ x
+  z ⊻ θ ⥅ x
   θ ⥅ y
   c
 end
