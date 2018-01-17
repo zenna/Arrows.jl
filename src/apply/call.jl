3f181e430d4efcf55f502107ed2cb6b3413c74aa
Javier Burroni <javier.burroni@gmail.com>
2018-01-17 18:13:43 -0500

    use a `Module` to interpret arrows

--- src/apply/call.jl
+++ src/apply/call.jl
@@ -4,6 +4,19 @@ function compile(arr::Arrow, target::Target) end
 "Adds arrow `arr` and any CompArrows is contains to global new space"
 compile!(arr::Arrow) = foreach(eval ∘ pol_to_julia, policies(arr))
 
+function do_compile_module(arr::CompArrow)
+  expr = compile(arr)
+  codes = expr.args[2].args[1:end-1]
+  return_stmt = expr.args[2].args[end]
+  names = return_stmt.args[1]
+  M = Module()
+  eval(M, :(using Arrows))
+  for code ∈ codes
+    eval(M, :($code))
+  end
+  eval(M, :($(names)[1]))
+end
+
 function do_compile(arr::CompArrow)
   lambda = eval(compile(arr))
   Base.invokelatest(lambda)[1]
@@ -11,11 +24,12 @@ end
 
 "Apply `arr(args...)``"
 function (arr::CompArrow)(args...)
-  f = do_compile(arr)
+  f = do_compile_module(arr)
   Base.invokelatest(f, args...)
 end
 
+
 "Convert `arr` into a julia function"
 function julia(arr::CompArrow)
-  f = do_compile(arr)
+  f = do_compile_module(arr)
 end
