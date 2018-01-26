6fce73350773c58267e8b3af9236b009f090d0ea
Zenna Tavares <zennatavares@gmail.com>
2018-01-26 13:29:03 -0500

    support assign expressions for grab

--- src/util/misc.jl
+++ src/util/misc.jl
@@ -147,7 +147,8 @@ function accumapply(f::Function, x::T) where T
   results = map(mthd -> invoke(f, Tuple{firstparam(mthd)}, x), allmethods)
 end
 
-setinmod!(s::Symbol, val, mod=Main) = eval(Main, :($s = $val))
+"mod.s = val"
+setinmod!(s::Symbol, val, mod=Main) = eval(mod, :($s = $val))
 
 "Global capture into Main
 
@@ -160,8 +161,35 @@ end
 
 x_grab
 "
-macro grab(var)
+macro grab(var::Symbol)
   @show var
   grabname = Symbol(var, :_grab)
   :(setinmod!($(Meta.quot(grabname)), $(esc(var))))
+end
+
+"""Global capture into Main
+
+```jldoctest
+julia> function f(x)
+         @grab x = 2x + 3
+         y = 3x*2
+       end
+f (generic function with 1 method)
+
+julia> f(10)
+138
+
+julia> x_grab
+23
+````
+x_grab
+"""
+macro grab(assignexpr::Expr)
+  @pre assignexpr.head == :(=) "Must be assignment expression"
+  var = assignexpr.args[1]
+  grabname = Symbol(var, :_grab)
+  quote
+    $(esc(assignexpr))
+    setinmod!($(Meta.quot(grabname)), $(esc(var)))
+  end
 end
