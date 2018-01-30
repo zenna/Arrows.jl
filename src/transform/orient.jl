2ac879ae348422ccb1145bca8861d94907e00243
Zenna Tavares <zennatavares@gmail.com>
2018-01-30 15:39:09 -0500

    orientation proto start

---
+++ src/transform/orient.jl
@@ -0,0 +1,10 @@
+# Relation direction/functionalization/graphing Given directions to relations
+
+"""Acyclically orient a relatiGeneralizes `invert`"""
+function orient(carr)
+end
+
+function isdag(carr. linkdirs) end
+
+"Cost associated with this linkdirs in carr"
+function cost(carr, linkdirs)::Integer end
